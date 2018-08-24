ea6818b0f4edbeee56aca2f3e8f14d911770a28c
HyeonJi Kim <m_0430@naver.com>
2018-08-24 09:38:33 +0000

    수정해커톤

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,6 +11,30 @@ class LikesController < ApplicationController
         five = params[:value5]
         six = params[:value6]
         
+        if one.nil?
+            one = "dfjkdlqjfdfa1"
+        end
+        
+        if two.nil?
+            two = "dfjkdlqjfdfa2"
+        end
+        
+        if three.nil?
+            three = "dfjkdlqjfdfa6"
+        end
+        
+        if four.nil?
+            four = "dfjkdlqjfdfa3"
+        end
+        
+        if five.nil?
+            five = "dfjkdlqjfdfa4"
+        end
+        
+        if six.nil?
+            six = "dfjkdlqjfdfa5"
+        end
+        
         if (one == two) || (one == three) || (one == four) || (one == five) || (one == six) || (two == three) || (two == four) || (two == five) || (two == six) || (three == four) || (three == five) || (three == six) || (four == five) || (four == six) || (five == six)
             flash[:fail] = "역할을 중복하여 선택하셨습니다."
             redirect_to "/projects/#{params[:project_id]}"
